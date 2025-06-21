9e0ce3561ce229d90941ead6ab1cda053c91e4f3
RmRCunza <lacunzaramirez@gmail.com>
2025-06-21 18:28:41 -0500

    Se agrego la interfaz Yamboly

---
+++ IntMenu.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace Tienda_Yamboly
+{
+    partial class IntMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(IntMenu));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnIntCli = new System.Windows.Forms.Button();
+            this.btnIntProd = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(598, 86);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Principal";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnIntProd);
+            this.groupBox2.Controls.Add(this.btnIntCli);
+            this.groupBox2.Location = new System.Drawing.Point(12, 414);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(598, 83);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Mantenedores";
+            // 
+            // btnIntCli
+            // 
+            this.btnIntCli.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(240)))), ((int)(((byte)(13)))));
+            this.btnIntCli.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(8)))), ((int)(((byte)(130)))));
+            this.btnIntCli.Location = new System.Drawing.Point(6, 21);
+            this.btnIntCli.Name = "btnIntCli";
+            this.btnIntCli.Size = new System.Drawing.Size(165, 52);
+            this.btnIntCli.TabIndex = 2;
+            this.btnIntCli.Text = "Cliente";
+            this.btnIntCli.UseVisualStyleBackColor = false;
+            // 
+            // btnIntProd
+            // 
+            this.btnIntProd.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(240)))), ((int)(((byte)(13)))));
+            this.btnIntProd.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(8)))), ((int)(((byte)(130)))));
+            this.btnIntProd.Location = new System.Drawing.Point(408, 21);
+            this.btnIntProd.Name = "btnIntProd";
+            this.btnIntProd.Size = new System.Drawing.Size(165, 52);
+            this.btnIntProd.TabIndex = 3;
+            this.btnIntProd.Text = "Producto";
+            this.btnIntProd.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(8)))), ((int)(((byte)(130)))));
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(240)))), ((int)(((byte)(13)))));
+            this.button1.Location = new System.Drawing.Point(6, 21);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(165, 52);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Pedido";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(8)))), ((int)(((byte)(130)))));
+            this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(240)))), ((int)(((byte)(13)))));
+            this.button2.Location = new System.Drawing.Point(203, 21);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(165, 52);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Venta";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(8)))), ((int)(((byte)(130)))));
+            this.button3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(240)))), ((int)(((byte)(13)))));
+            this.button3.Location = new System.Drawing.Point(408, 21);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(165, 52);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Pago";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(126, 104);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(339, 283);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // IntMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(194)))), ((int)(((byte)(233)))));
+            this.ClientSize = new System.Drawing.Size(623, 509);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "IntMenu";
+            this.Text = "IntMenu";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnIntProd;
+        private System.Windows.Forms.Button btnIntCli;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
